bbbd558dd182c607171103e2556bfb953ab7da5a
Shameem Ahammed <shameem105455@gmail.com>
2024-10-11 20:01:33 +0600

    newfiles

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-# Fake_news_detection
-Used model:
-Logistic Regression
-Random Forrest Classification
+##Fake news detector
+web application
+to open the flask project, your local machine must need to have the python installed.
 
-Used Dataset:
-Kagel Dataset
+to run this project:
+1. clone this repository
+2. open comand prompt to this directory 
+3. double click to app.py or in cmd type python app.py
+4. wait for sometime.
+5. you will get a link in the cmd like-> "http://127.0.0.1:5000"
+6. copy the link and pest it to the browser url section
+
+
+
+File structure:
+
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+# import packages
 from flask import Flask, render_template, request
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
@@ -5,26 +7,23 @@ import pandas as pd
 from sklearn.model_selection import train_test_split 
 import re
 import string 
+import os 
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 app=Flask(__name__)
-fake_news=pd.read_csv("Fake.csv")
-true_news=pd.read_csv("True.csv")
-fake_news["label"]=False
-true_news["label"]=True
-fake_manual_testing=fake_news.tail(1000)
-for i in range(23480,22480,-1):
-    fake_news.drop([i],axis=0,inplace=True)
-    
-true_manual_testing=true_news.tail(1000)
-for i in range(21416,20416,-1):
-    true_news.drop([i],axis=0,inplace=True)
 
-manual_testing=pd.concat([true_manual_testing, fake_manual_testing], axis=0)
+#  import pictures for webview
+picFolder = os.path.join('static','pic')
 
-marged_data=pd.concat([fake_news, true_news], axis=0)
-temp=marged_data.drop(["title","subject","date"], axis=1)
-temp=temp.sample(frac=1)
+app.config['UPLOAD_FOLDER'] = picFolder
+
+
+# Load both the saved model and the fitted vectorizer
+with open('model_and_vectorizer.pkl', 'rb') as f:
+    vectorization, model = pickle.load(f)
+
+
 
 def word_process(text):
     text=text.lower()
@@ -37,61 +36,58 @@ def word_process(text):
     text=re.sub('\w*\d\w*','',text)
     return text
 
-
-temp["text"]=temp["text"].apply(word_process)
-x=temp["text"]
-y=temp["label"]
-
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.25)
-###vectorize
-###from sklearn.feature_extraction.text import TfidfVectorizer
-vectorization =TfidfVectorizer()
-xv_train=vectorization.fit_transform(x_train)
-xv_test=vectorization.transform(x_test)
-
-###logistic regression
-from sklearn.linear_model import LogisticRegression
-LR=LogisticRegression()
-LR.fit(xv_train,y_train)
-LogisticRegression()
-predict_LR=LR.predict(xv_test)
-prob_LR=LR.predict_proba(xv_test)
-
-a=1.0;
+a=1.0
 def output_label(n):
     if n==False:
         return "Fake"
     else:
         return "True"
-
+# you can add the saved model but you need to do all the data processing for the input 
 def manual_testing(news):
     testing_news={"text":[news]}
     new_def_test=pd.DataFrame(testing_news)
     new_def_test["text"]=new_def_test["text"].apply(word_process)
     new_x_test=new_def_test["text"]
     new_xv_test= vectorization.transform(new_x_test)
-    predict_LR= LR.predict(new_xv_test)
-    prob_LR=LR.predict_proba(new_xv_test)
-    a=prob_LR[0][1]
-    print(a)
-    result=output_label(predict_LR)
+    predict_rfc=model.predict(new_xv_test)
+    prob_rfc=model.predict_proba(new_xv_test)
+    a=prob_rfc[0][1]
+    result=output_label(predict_rfc)
     return result
 
+twt = os.path.join(app.config['UPLOAD_FOLDER'],'twitter.png')
+fb = os.path.join(app.config['UPLOAD_FOLDER'],'facebook.png')
+ins = os.path.join(app.config['UPLOAD_FOLDER'],'instagram.png')
 
+# define the route
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html',img1=twt,fbp=fb,inst=ins) #pass the parameters
 
+@app.route('/about')
+def about():
+    return render_template('about.html',img1=twt,fbp=fb,inst=ins)
+@app.route('/predt')
+def predt():
+     return render_template('index.html',img1=twt,fbp=fb,inst=ins)
+
+@app.route('/cont')
+def cont():
+     return render_template('contact.html',img1=twt,fbp=fb,inst=ins)
+
+@app.route('/home2')
+def home2():
+     return render_template('home.html',img1=twt,fbp=fb,inst=ins)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
         message = request.form['message']
         pred = manual_testing(message)
-        print(pred)
-        return render_template('index.html', prediction=pred)
+
+        return render_template('index.html', prediction=pred,img1=twt,fbp=fb,inst=ins)
     else:
-        return render_template('index.html', prediction="Something went wrong")
+        return render_template('index.html', prediction="Something went wrong",img1=twt,fbp=fb,inst=ins)
 
 if __name__ == '__main__':
     app.run(debug=True)
